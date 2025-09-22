0fb83a5cbc9bb971a6d70357aed7586f3fb1ceb7
Dorian <dorianmuhametaj0920@gmail.com>
2025-09-22 17:31:01 -0400

    Made a constellation system that showed up at specified transform points. Was unable to make them gradually draw in.

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
+    public List<Vector2> starPositions;
     public float drawingTime;
+    public LineRenderer lineDraw; 
 
     // Update is called once per frame
     void Update()
     {
+        DrawConstellation(); 
+    }
+
+    public void DrawConstellation()
+    {
+
+        foreach (Transform starTransforms in starTransforms)
+        {
+            starPositions.Add(starTransforms.position);
+        }
+        lineDraw.positionCount = starPositions.Count();
+        for (int i = 0; i < starPositions.Count(); i++)
+        {
+            lineDraw.SetPosition(i, starPositions[i]);
+        }
+        int store = starPositions.Count(); 
+        for (int i = 0; i < store - 1; i++)
+        {
+            starPositions.Remove(starPositions[i - i]);
+        }
     }
 }
