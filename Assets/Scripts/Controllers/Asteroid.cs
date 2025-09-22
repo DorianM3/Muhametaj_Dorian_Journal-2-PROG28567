15e207124e7f985445ecd5256d506a52ee444c33
Dorian <dorianmuhametaj0920@gmail.com>
2025-09-22 01:53:15 -0400

    Attempted to make asteroids lerp in a random direction at a set speed smoothly, ended up making them vibrate a bit aggressively as an alternative as I was unable to complete the task

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.Apple;
 
 public class Asteroid : MonoBehaviour
 {
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+
+    [Range(0, 1)]
+    public float t; 
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +21,36 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+        int randomDir = Random.Range(0, 4);
+        Vector2 asteroidPos = transform.position;
+        Vector2 destination = asteroidPos; 
+
+        if (randomDir == 0)
+        {
+            destination += Vector2.up * (maxFloatDistance - arrivalDistance) * Time.deltaTime;
+        }
+
+        else if (randomDir == 1)
+        {
+            destination += Vector2.down * (maxFloatDistance - arrivalDistance) * Time.deltaTime;
+        }
+
+        else if (randomDir == 2)
+        {
+            destination += Vector2.left * (maxFloatDistance - arrivalDistance) * Time.deltaTime;
+        }
+
+        else
+        {
+            destination += Vector2.right * (maxFloatDistance - arrivalDistance) * Time.deltaTime;
+        }
+
+        for(float i = t; i <= 1; i += Time.deltaTime * moveSpeed)
+        transform.position = Vector2.Lerp(asteroidPos, destination, i); 
     }
 }
