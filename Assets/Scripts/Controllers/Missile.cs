f341e5cc1055adb6795e42be82d3a81c17b324a0
muhameta <muhameta@OA-S318-16>
2025-10-14 14:32:04 -0400

    Made it so the missiles will home in on the targets at a certain radius, destroying the asteroid and the missile itself

--- Assets/Scripts/Controllers/Missile.cs
+++ Assets/Scripts/Controllers/Missile.cs
@@ -1,14 +1,20 @@
-using System.Reflection;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    public Asteroid[] asteroids;
     public float speedOfMissile;
-    public float radiusOfDetection;
+    private float radiusOfDetection = 3f;
+    public bool homeIn = false;
+    public bool dontLook = false; 
+    private Transform storeAsteroid;
+    [Range(0,10)]
+    public float t;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        asteroids = FindObjectsByType<Asteroid>(FindObjectsSortMode.None);
     }
 
     // Update is called once per frame
@@ -22,6 +28,38 @@ public class Missile : MonoBehaviour
         Vector2 missilePos = transform.position;
         missilePos.y += Time.deltaTime * speed;
         transform.position = missilePos;
+
+        float currentClosest = radius;
+
+        for (int i = 0; i < asteroids.Length && dontLook == false; ++i)
+        {
+            float dist = Vector3.Distance(asteroids[i].transform.position, transform.position);
+            if (dist <= currentClosest && transform.position.y < asteroids[i].transform.position.y)
+            {
+                currentClosest = dist;
+                homeIn = true;
+                storeAsteroid = asteroids[i].transform;
+            }
+        }
+
+        if (homeIn)
+        {
+            dontLook = true;
+            t += Time.deltaTime; 
+            transform.position = Vector3.Lerp(transform.position, storeAsteroid.position, t / 2);
+
+            if (Vector3.Distance(transform.position, storeAsteroid.position) < 0.5f)
+            {
+                Destroy(gameObject);
+                Destroy(storeAsteroid.gameObject); 
+            }
+
+            if (missilePos.y > 10)
+            {
+                Destroy(gameObject);
+            }
+        }
+
 
         if (missilePos.y > 10)
         {
